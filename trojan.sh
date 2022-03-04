8f6223afd91b1b9b14707e51606dbb1dc0fa0309
azrilalkahfi <99373235+azrilalkahfi@users.noreply.github.com>
2022-03-04 08:52:50 +0700

    Add files via upload

---
+++ trojan.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear
+sleep 1
+figlet kek tai
+echo
+sleep 1
+echo -e "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e " AUTHOR : MR TAI AYAM"
+echo -e " TEAM : DARK CLUB LINKNYA (https://chat.whatsapp.com/GfDPRMb91AD8UXpD2jbJVD "
+echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo -e "1). SEND MALWARE"
+echo -e "2). SEND TAI AYAM ( TROJANS :V )"
+echo
+read -p "pilih>> " virus
+if [[ $virus == "1" ]]
+then
+read -p "masukkan nomor target => " nomor
+while [[ true ]]; do
+echo "SEND MALWARE ke $nomor telah berhasil"
+done
+fi
+if [[ $virus == "2" ]]
+then
+read -p "masukkan nomor target => " nomor2
+while [[ true ]]; do
+echo "SEND TAI AYAM ( trojans :v ) ke $nomor2 telah berhasil"
+done
+fi
